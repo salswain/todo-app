45ef9980cefb23b43a396330a8bab07035959d76
salvatoreswain <salswain63@gmail.com>
2023-08-31 15:13:52 -0400

    Implemented "Add" Button

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
-from functions import get_todos, write_todos
+# from functions import get_todos, write_todos
+import functions
 import time
 
 now = time.strftime("%b %d, %Y %H:%M:%S")
@@ -11,15 +12,15 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos(todos, "/Users/salvatoreswain/PycharmProjects/Todoapp/files/todos.txt")
+        functions.write_todos(todos, "/Users/salvatoreswain/PycharmProjects/Todoapp/files/todos.txt")
 
     elif user_action.startswith('show'):
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -31,12 +32,12 @@ while True:
             print(number)
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter a new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos)
+            functions.write_todos(todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -45,12 +46,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
